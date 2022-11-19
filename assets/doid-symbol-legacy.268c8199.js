56307b32b84786f2f922f48465a0d88689c80b88
Issac <Issac>
2022-11-19 18:59:52 +0800

    Debugger

---
+++ assets/doid-symbol-legacy.268c8199.js
@@ -0,0 +1,6 @@
+System.register(["./index-legacy.015f5a88.js"],(function(e,t){"use strict";var s,r,n;return{setters:[e=>{s=e.T,r=e.y,n=e.c}],execute:function(){var e=Object.defineProperty,t=Object.getOwnPropertyDescriptor;let o=class extends(s("")){render(){return r`<strong class="block w-24 h-24 m-12 mx-auto" href="/"
+        ><img class="w-full h-full object-contain select-none pointer-events-none" src="${"/assets/icon.15c8a698.svg"}"
+      /></strong>
+      <p class="my-8 text-center">
+        <strong><slot name="h1"></slot></strong>
+      </p>`}};o=((s,r,n,o)=>{for(var c,l=o>1?void 0:o?t(r,n):r,a=s.length-1;a>=0;a--)(c=s[a])&&(l=(o?c(r,n,l):c(l))||l);return o&&l&&e(r,n,l),l})([n("doid-symbol")],o)}}}));
