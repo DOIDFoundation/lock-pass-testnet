9e021813d91b5ef432c2be9de1d1db42a573b0a5
Issac <Issac>
2022-12-02 14:07:47 +0800

    Breaking Changes: - chg: Build from monorepo - feat: Support multi-apps

--- assets/doid-symbol-legacy.416765bb.js
+++
@@ -1,6 +0,0 @@
-System.register(["./index-legacy.9057db86.js"],(function(e,t){"use strict";var s,r,n;return{setters:[e=>{s=e.T,r=e.y,n=e.d}],execute:function(){var e=Object.defineProperty,t=Object.getOwnPropertyDescriptor;let o=class extends(s("")){render(){return r`<strong class="block w-24 h-24 m-12 mx-auto" href="/"
-        ><img class="w-full h-full object-contain select-none pointer-events-none" src="${"/assets/icon.15c8a698.svg"}"
-      /></strong>
-      <p class="my-8 text-center">
-        <strong><slot name="h1"></slot></strong>
-      </p>`}};o=((s,r,n,o)=>{for(var c,l=o>1?void 0:o?t(r,n):r,a=s.length-1;a>=0;a--)(c=s[a])&&(l=(o?c(r,n,l):c(l))||l);return o&&l&&e(r,n,l),l})([n("doid-symbol")],o)}}}));
--- assets/doid-symbol.29d53439.js
+++
@@ -1,6 +0,0 @@
-import{T as a,y as f,d as i}from"./index.f745aace.js";const m="/assets/icon.15c8a698.svg";var p=Object.defineProperty,v=Object.getOwnPropertyDescriptor,g=(l,s,r,n)=>{for(var e=n>1?void 0:n?v(s,r):s,t=l.length-1,o;t>=0;t--)(o=l[t])&&(e=(n?o(s,r,e):o(e))||e);return n&&e&&p(s,r,e),e};let c=class extends a(""){render(){return f`<strong class="block w-24 h-24 m-12 mx-auto" href="/"
-        ><img class="w-full h-full object-contain select-none pointer-events-none" src="${m}"
-      /></strong>
-      <p class="my-8 text-center">
-        <strong><slot name="h1"></slot></strong>
-      </p>`}};c=g([i("doid-symbol")],c);
